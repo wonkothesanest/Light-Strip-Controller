e3f170757f9ea41b7668ae90d513026d241d8fef
Dustin Hagstrom <dustin.hagstrom@gmail.com>
2016-04-03 16:21:19 -1000

    LampControl_v5_UltrasonicAndButton.ino LampControlColor.cpp LampControlColor.h

---
+++ LampControlColor.cpp
@@ -0,0 +1,8 @@
+#include "LampControlColor.h"
+
+Color::Color(int r, int g, int b){
+  this->red = r;
+  this->green = g;
+  this->blue = b;
+}
+
---
+++ LampControlColor.h
@@ -0,0 +1,19 @@
+// oohhh we get to make a class to store a color object
+
+#ifndef Color_h
+#define Color_h
+
+
+
+
+class Color {
+  public:
+    int red;
+    int green;
+    int blue;
+    Color(int r, int g, int b);
+
+};
+
+#endif
+
